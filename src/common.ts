42d264351e23a1b015bd10b870e0ea86a846dfc7
Daniel O'Brien <dan@dobs.org>
2021-11-15 11:55:43 -0500

    Switching to microbundle and storybook from tsdx.
    Hopefully resolves some of the issues resulting from not pointing to type definitions properly.

--- src/common.ts
+++ src/common.ts
@@ -1,14 +1,14 @@
-export type Align = 'center' | 'left' | 'right';
-export type VAlign = 'bottom' | 'center' | 'top';
+export type Align = "center" | "left" | "right";
+export type VAlign = "bottom" | "center" | "top";
 
 export const alignMap: { [_: string]: string } = {
-  center: 'space-around',
-  left: 'flex-start',
-  right: 'flex-end',
+  center: "space-around",
+  left: "flex-start",
+  right: "flex-end",
 };
 
 export const valignMap: { [_: string]: string } = {
-  bottom: 'flex-end',
-  center: 'center',
-  top: 'flex-start',
+  bottom: "flex-end",
+  center: "center",
+  top: "flex-start",
 };
