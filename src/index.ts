57cbdaee792334c37f45aae8f4cb15dd79f0213f
Daniel O'Brien <dan@dobs.org>
2021-11-14 22:50:22 -0500

    Migrating away from TSDX. (#310)
    At the moment this means microbundle for bundling, storytime for examples.

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Mapping to `Props` to avoid the major version bump.
+// TODO: Deprecate.
+import { PreserveRatioProps } from "./PreserveRatio";
+export type Props = PreserveRatioProps;
+
+export { PreserveRatio, PreserveRatioContext, useScale } from "./PreserveRatio";
+
+export { PreserveScale } from "./PreserveScale";
